559b796c7fe48de4b00aee4278c960ffe514bb49
Satnam <pandeykrishna443@gmail.com>
2018-01-24 15:16:21 +0530

    New

---
+++ newerpadmission/src/main/java/com/newadmission/newerpadmission/Base.java
@@ -0,0 +1,50 @@
+package com.newadmission.newerpadmission;
+
+import java.util.Set;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import com.newerpadmission.page.WebDriver;
+
+public class Base {
+	public static WebDriver driver;
+	
+	@Test
+	public void Login() throws InterruptedException {
+		  System.setProperty("webdriver.chrome.driver","D:\\SEL\\chromedriver.exe");
+		  ChromeDriver driver = new ChromeDriver();	 
+		  driver.get("http://qcerp.franciscansolutions.in/Secure/default.aspx");
+		  driver.findElement(By.id("txtUserName")).sendKeys("admin");
+		  driver.findElement(By.id("txtPassword")).sendKeys("admin@fspl");
+		  driver.findElement(By.id("btnLogin")).click();
+		  
+		  			String Parentwindow=driver.getWindowHandle();
+		  System.out.println(Parentwindow);
+		  driver.findElement(By.xpath("//*[@id=\"form1\"]/div[4]/div/div/div[1]/div/div[2]/div[1]/div[4]/a")).click();
+		  
+		  			Set<String> menulist= driver.getWindowHandles();
+		  System.out.println(menulist);
+		  Thread.sleep(200);
+				  
+				  for(String A : menulist)
+				  {
+					  if(!Parentwindow.equalsIgnoreCase(A))
+					  {
+						  driver.switchTo().window(A);
+					  }
+				  }
+		  WebElement searchBtn = driver.findElement(By.xpath("//*[@id=\"form2\"]/div[4]/div/ul/li[4]/a"));
+				  
+
+		 Actions action = new Actions(driver);
+		  action.moveToElement(searchBtn).perform();   
+		 action.moveToElement(searchBtn).moveToElement(driver.findElement(By.xpath("//*[@id=\"form2\"]/div[4]/div/ul/li[3]/ul/li[1]/a"))).click().build().perform();
+	}
+	
+	
+
+}
---
+++ newerpadmission/src/main/java/com/newerpadmission/page/WebDriver.java
@@ -0,0 +1,13 @@
+package com.newerpadmission.page;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class WebDriver {
+
+	public WebElement findElement(By xpath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
